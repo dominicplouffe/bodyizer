2f11901fc7b470b07897e050b67936589f55c74e
dominicplouffe <dominic@dplouffe.ca>
2014-08-03 13:57:28 -0400

    added web folder

---
+++ web/http/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import locale
+
+# import auth
+# import bookmark
+
+locale.setlocale(locale.LC_ALL, 'en_US')
+
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+# app.register_blueprint(auth.auth, url_prefix='/api/v1.0/auth')
+# app.register_blueprint(bookmark.bookmark, url_prefix='/api/v1.0/bookmark')
+# app.register_blueprint(gpx.gpx, url_prefix='/gpx')
+# app.register_blueprint(finances.finances, url_prefix='/finances')
+
+app.secret_key = '1qaz2wsx!'
+
+@app.route('/')
+def home_page_view():
+    return render_template(
+        'home.html',
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
